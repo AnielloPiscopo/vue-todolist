5f88832ca1ca2039dc37d8286eec2ee215b17cf8
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-14 15:20:47 +0100

    Added other initial elements

--- js/script.js
+++ js/script.js
@@ -6,7 +6,59 @@ const {createApp} = Vue;
 
 createApp({
     data(){
-        return{}
+        return{
+            toDoActions : [
+                {
+                    text:'Fare i compiti',
+                    done: false,
+                },
+
+                {
+                    text:'Fare la spesa',
+                    done: true,
+                },
+
+                {
+                    text:'Portare il cane a passeggio',
+                    done: false,
+                },
+
+                {
+                    text:'Cambiare la busta della spazzatura',
+                    done: true,
+                },
+
+                {
+                    text:'Cambiare la ciotola al cane',
+                    done: true,
+                },
+
+                {
+                    text:'Dare il cibo ai gatti',
+                    done: false,
+                },
+
+                {
+                    text:'Cucinare',
+                    done: true,
+                },
+
+                {
+                    text:'Studiare per l\'esame',
+                    done: true,
+                },
+
+                {
+                    text:'Andare in palestra',
+                    done: false,
+                },
+
+                {
+                    text:'Fare la ricarica telefonica',
+                    done: false,
+                },
+            ]
+        }
     },
 
 
@@ -14,4 +66,4 @@ createApp({
     methods: {
         
     },
-})
+}).mount('#app')
